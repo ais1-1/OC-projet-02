92682c5d9a4448f2bf22874b304ac26506f3cc3a
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2021-12-02 15:27:14 +0100

    download and save image file of each products and more
    - write comments in English
    - add base_url as a parameter for complete links
    - clean unwanted spaces

--- P2_01_extract-product-data.py
+++ P2_01_extract-product-data.py
@@ -1,53 +1,58 @@
-## SPDX-FileCopyrightText: 2021 Aiswarya Kaitheri Kandoth
-## SPDX-License-Identifier: GPL-3.0-or-later
+##
+# SPDX-FileCopyrightText: 2021 Aiswarya Kaitheri Kandoth
+#
+#Authors :
+# Aiswarya Kaitheri Kandoth
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+##
 
+import os
 import requests
 from bs4 import BeautifulSoup
 import re
 import csv
+import shutil
 
 ## function to create soup from a url
 def creer_soup(url):
     reponse = requests.get(url)
     html_page = reponse.content
 
-    # transforme en objet BeautifulSoup
+    # transform to BeautifulSoup object
     soup = BeautifulSoup(html_page, 'html.parser')
     return soup
 
 ## function to extract product details from a product page
-def scrapper_un_page_produit(product_page_url):
+def scrapper_un_page_produit(product_page_url, base_url):
     data = []
     data.append(product_page_url)
-    #print("product page url is", product_page_url)
 
-    #parse le page HTML en objet Beautifulsoup
+    # create soup of the product page
     soup = creer_soup(product_page_url)
 
-    # extraire des ligne de la table
+    # extract table rows
     table_rows = soup.find_all("table", class_="table")[0].find_all("tr")
     second_column = []
-    # faire un array avec les contenus de la deuxieme colonne
+    # make an array with contents from 2nd column
     for row in table_rows:
         second_column.append(row.find_all('td')[0].text)
-    # acceder le universal_product_code(upc)
+    # access the universal_product_code(upc)
     upc = second_column[0]
     data.append(upc)
-    #print("upc is ", upc)
 
+    # access title
     title = soup.find("h1").string
     data.append(title)
-    #print("title is ", title)
 
+    # access price including and excluding taxes
     price_including_tax = second_column[3]
     data.append(price_including_tax)
-    #print("price including tax is ", price_including_tax)
 
     price_excluding_tax = second_column[2]
     data.append(price_excluding_tax)
-    #print("price excluding tax is ", price_excluding_tax)
 
-    ## function pour recuperer l'entier depuis une phrase (remplacé par regex re.search)
+    ## function for geting integers from a sentence (replaced by regex re.search)
     '''def get_int_from_string(availability):
         list_of_words = availability.split("(")[1].split()
         for i in list_of_words:
@@ -64,7 +69,6 @@ def scrapper_un_page_produit(product_page_url):
     et int() transforment le résultat entier en nombre entier.'''
     number_available = int(re.search(r'\d+', availability_detail).group())
     data.append(number_available)
-    #print("number available is ", number_available)
 
     # get all meta tags
     all_metas = soup.find_all('meta')
@@ -73,29 +77,58 @@ def scrapper_un_page_produit(product_page_url):
             #if the attribute 'name' is description assign the attribute 'content' to product description
             product_description = meta.attrs['content']
     data.append(product_description)
-    #print("description is ", product_description)
 
     #get the ul part with class breadcrumb
     ul_elements = soup.find('ul', class_='breadcrumb')
     # get Category, which is the third li inside ul
     category = ul_elements.find_all('li')[2].text
     data.append(category)
-    #print("category is ", category)
 
     #get the part with star rating
     star_ratings = soup.find('p', class_='star-rating')
     #get the second class name which indicates the number of stars
     review_rating = star_ratings['class'][1]
     data.append(review_rating)
-    #print("review rating is", review_rating, "stars")
 
     #get the image tag
     image_tag = soup.find('img')
     #get the image url from the above result
     image_incomplete_url = image_tag['src']
-    image_url = re.sub("\../..", product_page_url, image_incomplete_url)
+    image_url = re.sub("\../..", base_url, image_incomplete_url)
     data.append(image_url)
-    #print("image url is", image_url)
+
+    ## download images from each product urls visited
+
+    # use os module to create folders to store image files if it does not exists
+    # path to the main image folder
+    path_to_image_folder = os.path.join("product_images")
+    # path to category folder inside the image folder
+    # replace white spaces with underscore for cleaner file names
+    category_folder_name = re.sub(" ", "_", category)
+    path_to_category_folder = os.path.join(path_to_image_folder, category_folder_name)
+
+    if not os.path.exists(path_to_image_folder):
+        os.mkdir(path_to_image_folder)
+
+    # create folder for each category if it doesn't exist
+    if not os.path.exists(path_to_category_folder):
+        os.mkdir(path_to_category_folder)
+
+    # get the file name with hash from image url which avoids same names of files
+    image_file_name = re.split("\/", image_url)[-1]
+    file_path = os.path.join(path_to_category_folder, image_file_name)
+
+    # get streamed response; if stream is false content will be downloaded in the memory before returning
+    image_reponse = requests.get(image_url, stream=True)
+
+    # if requests is a success (200) do writing
+    if image_reponse.status_code == 200:
+        # open file in binary mode (b) for writing (w) the response
+        with open(file_path, 'wb') as image_file:
+            # decode the raw content
+            image_reponse.raw.decode_content = True
+            # save the image to the machine; shutil copy the content of the raw object to image_file object
+            shutil.copyfileobj(image_reponse.raw, image_file)
 
     return data
 
@@ -118,9 +151,9 @@ def get_product_links_from_page(soup_de_page):
 
 ## function to extract product links from a category page
 def scrapper_page_category(category_page_url):
-    # url d'une category à scrapper
+    # create soup for a url of a category to extract
     soup_category = creer_soup(category_page_url)
-    #initialise product-links array
+    # initialise product-links array
     product_links = []
     product_links = get_product_links_from_page(soup_category)
 
@@ -145,18 +178,17 @@ def scrapper_page_category(category_page_url):
             product_links.extend(get_product_links_from_page(soup_category_next_page))
             # get next page
             next_page = soup_category_next_page.find('li', class_='next')
-    #print("product links of the category ", product_links)
 
     return product_links
 
-def create_csv_for_a_category(name_of_the_file, link_to_the_category_page):
-    # créer une liste des en-têtes
+def create_csv_for_a_category(name_of_the_file, link_to_the_category_page, base_url):
+    # create a list of headers (en-têtes)
     en_tete = ["product_page_url", "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
 
     # replace white spaces with underscore
     name_of_the_file_without_space =re.sub(" ", "_", name_of_the_file)
 
-    # créer fichier csv , write category name and write title row (en_tete) in it
+    # create csv file , write category name and write headers (en_tete) in it
     with open(name_of_the_file_without_space, 'w') as fichier_csv:
         writer = csv.writer(fichier_csv, delimiter=',')
         writer.writerow(en_tete)
@@ -164,11 +196,11 @@ def create_csv_for_a_category(name_of_the_file, link_to_the_category_page):
     # append data of each product in a category
     for link in link_to_the_category_page:
         category_product_data = []
-        category_product_data = scrapper_un_page_produit(link)
+        category_product_data = scrapper_un_page_produit(link, base_url)
         append_row_fichier_csv(name_of_the_file_without_space, category_product_data)
 
 def etl():
-    # url de la page à scrapper
+    # url of the page to scrape
     base_url = "http://books.toscrape.com"
     soup = creer_soup(base_url)
 
@@ -204,7 +236,6 @@ def etl():
     # extract products data for each category in the website
     for item in dictionary_for_categories:
         links_from_categories = scrapper_page_category(dictionary_for_categories[item])
-        create_csv_for_a_category(item, links_from_categories)
-
+        create_csv_for_a_category(item, links_from_categories, base_url)
 
 etl()
